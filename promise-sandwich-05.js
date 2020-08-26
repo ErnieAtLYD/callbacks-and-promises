bbf6b18c9fb72e19f7a23a8c2615a6eae3c29b17
Ernie Hsiung <ernie@wyncode.co>
2020-08-26 15:14:51 -0400

    with more notes

--- promise-sandwich-05.js
+++ promise-sandwich-05.js
@@ -35,3 +35,28 @@ function getSandwich() {
 }
 
 getSandwich();
+
+function job() {
+  return new Promise(function (resolve, reject) {
+    reject();
+  });
+}
+
+let promise = job();
+
+promise
+  .then(function () {
+    console.log("Success 1");
+  })
+  .then(function () {
+    console.log("Success 2");
+  })
+  .then(function () {
+    console.log("Success 3");
+  })
+  .catch(function () {
+    console.log("Error 1");
+  })
+  .then(function () {
+    console.log("Success 4");
+  });
